b5028b78bebd4b05a84e78fd0c13c81d5d842e9b
Sebastian Hönel <development@hoenel.net>
2018-04-02 12:46:08 +0200

    Add new tools-namespace/folder and a first file containing tools to create deferred Promises, also for Mocha

---
+++ tools/Defer.js
@@ -0,0 +1,54 @@
+require('../docs');
+
+
+/**
+ * Creates a deferred Promise and returns an object with function
+ * that can be used to resolve or reject the deferred.
+ * 
+ * @returns {Deferred}
+ */
+const defer = () => {
+  let _resolve = null;
+  let _reject = null;
+
+  const promise = new Promise((resolve, reject) => {
+    _resolve = resolve;
+    _reject = reject;
+  });
+
+  return {
+    promise,
+    resolve: _resolve,
+    reject: _reject
+  };
+};
+
+
+/**
+ * Mocha does not support this use case (async function with done):
+ * https://github.com/mochajs/mocha/issues/2407#issuecomment-237408877
+ * This function creates a deferred that can be resolved/rejected using
+ * a generic done-callback.
+ * 
+ * @returns {Array.<Promise<any>, (error?: any) => any>} an array where
+ * the first element is the promise that can be returned to mocha's test
+ * runner and the second element is the done-function, which, when called
+ * with no arguments, resolves the promise. Otherwise, it will reject the
+ * promise.
+ */
+const deferMocha = () => {
+  const deferred = defer();
+
+  return [deferred.promise, error => {
+    if (error === void 0) {
+      deferred.resolve();
+    } else {
+      deferred.reject(error);
+    }
+  }];
+};
+
+module.exports = Object.freeze({
+  defer,
+  deferMocha
+});
